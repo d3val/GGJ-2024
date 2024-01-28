086c1c3b5cec106b5c2b96109932fb5cf3df7ce7
d3val <devalt5008@gmail.com>
2024-01-28 09:56:13 -0600

    Player animations added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,6 +12,8 @@ public class PlayerMovement : MonoBehaviour
     InputAction XInput;
     Vector2 direction;
     [SerializeField] float speed = 5f;
+    [SerializeField] Animator animator;
+    [SerializeField] SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Awake()
@@ -21,7 +23,7 @@ public class PlayerMovement : MonoBehaviour
         XInput = playerActionMap.FindAction("XMove");
 
         YInput.performed += ReadYValue;
-        XInput.performed += ReadXValue; 
+        XInput.performed += ReadXValue;
         YInput.canceled += ReadYValue;
         XInput.canceled += ReadXValue;
     }
@@ -35,16 +37,27 @@ public class PlayerMovement : MonoBehaviour
 
     void Move()
     {
+        if (direction.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (direction.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+
         transform.Translate(Time.deltaTime * speed * direction.normalized);
     }
 
     void ReadYValue(InputAction.CallbackContext ctx)
     {
         direction.y = ctx.ReadValue<float>();
+        animator.SetFloat("YAxis", direction.y);
     }
     void ReadXValue(InputAction.CallbackContext ctx)
     {
         direction.x = ctx.ReadValue<float>();
+        animator.SetFloat("XAxis", Mathf.Abs(direction.x));
     }
 
     private void OnEnable()
