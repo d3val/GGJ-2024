f9ec34dff7f436fc979f564c6854b413c8616958
rubenlam <rubenlam_do@hotmail.com>
2024-01-27 22:37:46 -0600

    UI nivel1 (#5)
    * Dialogue UI
    
    * UI Adjustment
    
    * Faces Change Script

---
+++ Assets/Scripts/UI/Characters/ProtagonistBehavior.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProtagonistBehavior : MonoBehaviour
+{
+    [SerializeField] ProtagonistBehavior m_Behavior;
+    public GameObject [] faces;
+    private GameObject actualFace;
+    private int facesAmount;
+    public  int desireFace;
+    private int a;
+
+    public void HideActualFace(GameObject OldFace)
+    {
+        OldFace.SetActive(false);
+    }
+    public void ShowNewFace(GameObject newFace)
+    {
+        faces[desireFace].SetActive(true);
+    }
+
+    //Función de cambio 
+    public void ChangeFace(GameObject face)
+    {
+        HideActualFace(actualFace);
+        ShowNewFace(face);
+        actualFace = faces[desireFace];
+    }
+    void Start()
+    {
+        facesAmount = faces.Length;
+        actualFace = faces[0];
+        ShowNewFace(actualFace);
+
+        //variable para comprobación para cambio de caras
+        a = 0;
+    }
+    
+    // Método de comprobación para cambio de caras
+    void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(desireFace < 2)
+            {
+                a++;
+                desireFace = a;
+                ChangeFace(faces[desireFace]);
+                Debug.Log(a);
+                Debug.Log(desireFace);
+            }
+            else
+            {
+                a = 0;
+                desireFace = a;
+                ChangeFace(faces[desireFace]);
+            }
+        }
+    }
+}
