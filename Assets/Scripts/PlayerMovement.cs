a6c336bee3fd963a90c91ac6f88bc467f8bc3283
d3val <90423330+d3val@users.noreply.github.com>
2024-01-28 11:03:29 -0600

    Friend interactions (#14)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,10 +10,13 @@ public class PlayerMovement : MonoBehaviour
     InputActionMap playerActionMap;
     InputAction YInput;
     InputAction XInput;
+    InputAction intertaction;
     Vector2 direction;
     [SerializeField] float speed = 5f;
     [SerializeField] Animator animator;
     [SerializeField] SpriteRenderer spriteRenderer;
+
+    Friend currentFriend;
 
     // Start is called before the first frame update
     void Awake()
@@ -21,11 +24,13 @@ public class PlayerMovement : MonoBehaviour
         playerActionMap = primatyActions.FindActionMap("Player");
         YInput = playerActionMap.FindAction("YMove");
         XInput = playerActionMap.FindAction("XMove");
+        intertaction = playerActionMap.FindAction("Interaction");
 
         YInput.performed += ReadYValue;
         XInput.performed += ReadXValue;
         YInput.canceled += ReadYValue;
         XInput.canceled += ReadXValue;
+        intertaction.performed += Interact;
     }
 
     // Update is called once per frame
@@ -60,16 +65,44 @@ public class PlayerMovement : MonoBehaviour
         animator.SetFloat("XAxis", Mathf.Abs(direction.x));
     }
 
+    void Interact(InputAction.CallbackContext ctx)
+    {
+        if (currentFriend == null)
+            return;
+
+        if (currentFriend.isTalking)
+        {
+            return;
+        }
+
+        currentFriend.StartSpeech();
+    }
+
     private void OnEnable()
     {
         XInput.Enable();
         YInput.Enable();
+        intertaction.Enable();
     }
 
     private void OnDisable()
     {
         XInput.Disable();
         YInput.Disable();
+        intertaction.Disable();
     }
 
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Friend"))
+        {
+            currentFriend = collision.GetComponent<Friend>();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        currentFriend = null;
+    }
 }
